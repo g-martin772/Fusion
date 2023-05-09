953c0dcf1418b8da25472e351a42a9a35d4a5cce
Gabriel Martin <g.martin@htlkrems.at>
2023-05-09 22:17:10 +0200

    Creating Vulkan Instance

--- FusionEngine/src/Platform/Vulkan/VulkanRenderApi.cpp
+++ FusionEngine/src/Platform/Vulkan/VulkanRenderApi.cpp
@@ -1,9 +1,12 @@
 ﻿#include "fepch.h"
 #include "VulkanRenderApi.h"
 
+#include "GLFW/glfw3.h"
+
 void VulkanRenderApi::Init()
 {
     FE_INFO("Initializing Vulkan");
+    CreateInstance();
 }
 
 void VulkanRenderApi::OnWindowResize(uint32_t width, uint32_t height)
@@ -12,4 +15,51 @@ void VulkanRenderApi::OnWindowResize(uint32_t width, uint32_t height)
 
 void VulkanRenderApi::ShutDown()
 {
+    m_Instance.destroy();
+}
+
+void VulkanRenderApi::CreateInstance()
+{
+    // https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap4.html#initialization-instances
+    // https://registry.khronos.org/vulkan/specs/1.3-extensions/html/chap44.html#extendingvulkan-coreversions-versionnumbers
+    uint32_t version;
+    vk:vkEnumerateInstanceVersion(&version);
+
+    vk::ApplicationInfo appInfo(
+        "Fusion Engine",
+        version,
+        "Fusion Engine",
+        version,
+        version);
+
+    // Get GLFW Window Extensions
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+    #ifdef FE_DEBUG
+    FE_INFO("Requesting following Vulkan extensions:");
+    for (const char * extension : extensions)
+        FE_INFO("\t{0}", extension);
+    #endif
+    
+    
+    vk::InstanceCreateInfo createInfo(
+        vk::InstanceCreateFlags(),
+        &appInfo,
+        0,
+        nullptr,
+        static_cast<uint32_t>(extensions.size()),
+        extensions.data());
+
+    try
+    {
+        FE_INFO("Creating Vulkan Instance...");
+        m_Instance = vk::createInstance(createInfo);
+        FE_INFO("Success");
+    }
+    catch (vk::SystemError error)
+    {
+        FE_ASSERT(false, "Failed to create Vulkan Instance");
+    }
 }
--- FusionEngine/src/Platform/Vulkan/VulkanRenderApi.h
+++ FusionEngine/src/Platform/Vulkan/VulkanRenderApi.h
@@ -1,10 +1,16 @@
 ﻿#pragma once
 #include "Renderer/RenderApi.h"
 
+#include <vulkan/vulkan.hpp>
+
 class VulkanRenderApi : public FusionEngine::RenderApi
 {
 public:
     void Init() override;
     void OnWindowResize(uint32_t width, uint32_t height) override;
     void ShutDown() override;
+private:
+    void CreateInstance();
+private:
+    vk::Instance m_Instance;
 };
