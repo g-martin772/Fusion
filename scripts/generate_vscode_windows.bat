15aecbadaf41bf5bab79812fab2cc3beaf932250
Gabriel Martin <g.martin@htlkrems.at>
2024-02-27 18:52:02 +0100

    ditch premake

--- scripts/generate_vscode_windows.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd %~dp0\..\
-call scripts\Dependencies\premake-5.0.0-beta2-windows\premake5.exe gmake2
-call scripts\Dependencies\premake-5.0.0-beta2-windows\premake5.exe vscode
-popd
-pause
